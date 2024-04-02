ecf16c73c9fa0009b054565917f4b8bba0bdffb6
leokazuyukinagatani <lknlkn15@hotmail.com>
2024-04-01 22:37:00 -0300

    chore: loading component customized

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,7 +1,27 @@
 import { ActivityIndicator } from "react-native";
+import clsx from "clsx";
 
-export function Loading({size = 48}) {
+type Props = {
+  size?: number;
+  color?: "green" | "orange" | "white" | "red";
+};
+export function Loading({ size = 48, color = "orange" }: Props) {
+  const indicatorColor = color === "white" ? "white" : color;
+
+  const indicatorClassName = clsx({
+    "text-green-500": indicatorColor === "green",
+    "text-orange-500": indicatorColor === "orange",
+    "text-white": indicatorColor === "white",
+    "text-red-500": indicatorColor === "red",
+  });
+  
   return (
-    <ActivityIndicator size={size} className="flex-1 bg-green-500 items-center justify-center text-orange-500" />
+    <ActivityIndicator
+      size={size}
+      className={clsx(
+        "flex-1 bg-transparent items-center justify-center",
+        indicatorClassName
+      )}
+    />
   );
 }
