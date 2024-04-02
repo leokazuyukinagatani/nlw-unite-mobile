9dffa6141d098e61adf90e813cdb9a62f0cea40c
leokazuyukinagatani <lknlkn15@hotmail.com>
2024-04-01 22:49:56 -0300

    chore: touchableOpacityProps in type

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,20 +1,21 @@
-import { Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { Loading } from "./loading";
 
-type Props = {
+type Props = TouchableOpacityProps & {
   title: string;
   isLoading?: boolean;
 };
 
-export function Button({ title, isLoading = false }: Props) {
+export function Button({ title, isLoading = false, ...rest }: Props) {
   return (
     <TouchableOpacity
       activeOpacity={0.7}
       disabled={isLoading}
       className="w-full h-14 bg-orange-500 items-center justify-center rounded-lg"
+      {...rest}
     >
       {isLoading ? (
-        <Loading color="green" size={24}/>
+        <Loading color="green" size={24} />
       ) : (
         <Text className="text-green-500 text-base font-bold uppercase">
           {title}
